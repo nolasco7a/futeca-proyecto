254d8e3d408ceac096d833c9811783d10a8a6ab7
cybarahona <47421973+cybarahona@users.noreply.github.com>
2020-02-11 10:38:37 -0600

    Cambios menores modal

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -8,52 +8,59 @@
         </div>
     </section>
     
-    <section class="py-4">
-        <div class="container">
-            <div class="row py-3">
-                @foreach ($noticias as $noticia)
-                <div class="col-lg-4 col-md-4 col-sm-6 col-12">
-                    <div class="card p-3 card-shadow">
-                    <img src="{{Storage::url($noticia->imagen)}}" class="card-img-top" alt="...">
-                      <div class="card-body p-0">
-                      <h2 class="card-date">{{$noticia->created_at}}</h2>
-                      <h5 class="card-title p-1 text-center text-bold">{{$noticia->titulo}}</h5>
-                        <div class="separator-title"></div>
-                        @php
-                            $descripcion = $noticia->descripcion;
-                            $descripcion = substr($descripcion, 0, 80);
-                        @endphp
-                      <p class="card-text">{{$descripcion}}...</p>
-                        <a href="" class="btn-futeca-2 d-flex" data-toggle="modal" data-target="#Modal{{$noticia->id}}">
-                          <div class="ml-2 mt-1">VER MÁS</div>
-                          <div class="after"></div>
-                          <span>></span>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                @endforeach
-            </div>
-        </div>
+    {{-- Seccion de ultimas noticias --}}
+<section>
+  <br><br><br>
+  <h1 class="text-center"><span class="h1-span">ÚLTIMAS</span> NOTICIAS</h1>
+  <div class="container">
+   <div class="row py-3">
         @foreach ($noticias as $noticia)
-    <div class="modal fade" id="Modal{{$noticia->id}}" tabindex="-1" role="dialog" aria-labelledby="ModalLabel{{$noticia->id}}" aria-hidden="true">
-            <div class="modal-dialogn modal-lg" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                <h5 class="modal-title" id="ModalLabel{{$noticia->id}}">{{$noticia->titulo}}</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                <p>{{$noticia->descripcion}}</p>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                </div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+            <div class="card p-3 card-shadow">
+            <img src="{{Storage::url($noticia->imagen)}}" class="card-img-top" alt="...">
+              <div class="card-body p-0">
+              <h2 class="card-date">{{$noticia->created_at}}</h2>
+              <h5 class="card-title p-1 text-center text-bold">{{$noticia->titulo}}</h5>
+                <div class="separator-title"></div>
+                @php
+                    $descripcion = $noticia->descripcion;
+                    $descripcion = substr($descripcion, 0, 80);
+                @endphp
+              <p class="card-text">{{$descripcion}}...</p>
+              <a href="" class="btn-futeca-2 d-flex" data-toggle="modal" data-target="#Modal{{$noticia->id}}">
+                <div class="ml-2 mt-1">VER MÁS</div>
+                <div class="after"></div>
+                <span>></span>
+              </a>
               </div>
             </div>
           </div>
         @endforeach
-    </section>
+        @foreach ($noticias as $noticia)
+        <div class="modal fade" id="Modal{{$noticia->id}}" tabindex="-1" role="dialog" aria-labelledby="ModalLabel{{$noticia->id}}" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                    <h5 class="modal-title" id="ModalLabel{{$noticia->id}}">{{$noticia->titulo}}</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                    <p>{{$noticia->descripcion}}</p>
+                    </div>
+                    <div class="modal-footer">
+                      {{-- <a href="" class="btn-futeca-2 d-flex" data-dismiss="modal">
+                        <div class="ml-2 mt-1">CERRAR</div>
+                        <div class="after"></div>
+                        <span>></span>
+                      </a> --}}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            @endforeach
+    </div>
+</div>
+</section>
 @endsection
